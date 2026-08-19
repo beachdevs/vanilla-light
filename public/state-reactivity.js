4424199c3f8cc232ede9491315d91244d55a408b
digplan <digplan@users.noreply.github.com>
2026-08-18 19:07:38 -0500

    Implement state reactivity with Proxy

---
+++ public/state-reactivity.js
@@ -0,0 +1,15 @@
+globalThis.app = globalThis.app || {};
+
+app.state = new Proxy({}, {
+  set(o, p, v) {
+    o[p] = v;
+
+    $$(`[data="${p}"]`).forEach(el => {
+      if (typeof el.render === 'function') {
+        el.render(v);
+      }
+    });
+
+    return true;
+  }
+});
